fe1c269387283f157533eece5ff03f32653c3f61
Philipp Douglas <78768376+philippdouglas@users.noreply.github.com>
2023-08-21 19:13:20 -0300

    Update RelatorioController.cs
    Gráfico por Nome de os Produtos

--- Controllers/RelatorioController.cs
+++ Controllers/RelatorioController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,37 +22,40 @@ namespace SupplyChain.Controllers
 
         public IActionResult GetChartData()
         {
-            var entradasPorMes = _context.Entradas
-                .GroupBy(e => new { e.DataHora.Year, e.DataHora.Month })
+            var entradasPorMesPorProduto = _context.Entradas
+                .GroupBy(e => new { e.DataHora.Year, e.DataHora.Month, e.Mercadoria.Nome })
                 .Select(g => new
                 {
                     Year = g.Key.Year,
                     Month = g.Key.Month,
+                    Nome = g.Key.Nome,
                     Quantidade = g.Sum(e => e.Quantidade)
                 })
                 .OrderBy(g => g.Year)
                 .ThenBy(g => g.Month)
                 .ToList();
 
-            var saidasPorMes = _context.Saidas
-                .GroupBy(s => new { s.DataHora.Year, s.DataHora.Month })
+            var saidasPorMesPorProduto = _context.Saidas
+                .GroupBy(s => new { s.DataHora.Year, s.DataHora.Month, s.Mercadoria.Nome })
                 .Select(g => new
                 {
                     Year = g.Key.Year,
                     Month = g.Key.Month,
+                    Nome = g.Key.Nome,
                     Quantidade = -g.Sum(s => s.Quantidade) // Invert the quantity for the output (negative value)
                 })
                 .OrderBy(g => g.Year)
                 .ThenBy(g => g.Month)
                 .ToList();
 
-            var entradasESaidasPorMes = entradasPorMes
-                .Concat(saidasPorMes)
-                .GroupBy(es => new { es.Year, es.Month })
+            var entradasESaidasPorMesPorProduto = entradasPorMesPorProduto
+                .Concat(saidasPorMesPorProduto)
+                .GroupBy(es => new { es.Year, es.Month, es.Nome })
                 .Select(g => new
                 {
                     Year = g.Key.Year,
                     Month = g.Key.Month,
+                    Nome = g.Key.Nome,
                     Entradas = g.Sum(es => es.Quantidade >= 0 ? es.Quantidade : 0),
                     Saidas = g.Sum(es => es.Quantidade < 0 ? es.Quantidade : 0)
                 })
@@ -60,15 +63,37 @@ namespace SupplyChain.Controllers
                 .ThenBy(g => g.Month)
                 .ToList();
 
-            var labels = entradasESaidasPorMes.Select(g => new DateTime(g.Year, g.Month, 1).ToString("MMM yyyy")).ToList();
-            var entradasData = entradasESaidasPorMes.Select(g => g.Entradas).ToList();
-            var saidasData = entradasESaidasPorMes.Select(g => g.Saidas).ToList();
+            var uniqueProductNames = entradasESaidasPorMesPorProduto.Select(g => g.Nome).Distinct().ToList();
 
-            var datasets = new List<ChartDataset>
+            var labels = entradasESaidasPorMesPorProduto.Select(g => new DateTime(g.Year, g.Month, 1).ToString("MMM yyyy")).ToList();
+            var datasets = new List<ChartDataset>();
+
+            foreach (var productName in uniqueProductNames)
             {
-                new ChartDataset { Label = "Entradas", BackgroundColor = "red", Data = entradasData },
-                new ChartDataset { Label = "Saídas", BackgroundColor = "green", Data = saidasData }
-            };
+                var entradasData = entradasESaidasPorMesPorProduto
+                    .Where(g => g.Nome == productName)
+                    .Select(g => g.Entradas)
+                    .ToList();
+
+                var saidasData = entradasESaidasPorMesPorProduto
+                    .Where(g => g.Nome == productName)
+                    .Select(g => g.Saidas)
+                    .ToList();
+
+                datasets.Add(new ChartDataset
+                {
+                    Label = $"{productName} - Entradas",
+                    BackgroundColor = "red",
+                    Data = entradasData
+                });
+
+                datasets.Add(new ChartDataset
+                {
+                    Label = $"{productName} - Saídas",
+                    BackgroundColor = "green",
+                    Data = saidasData
+                });
+            }
 
             var chartData = new ChartData
             {
@@ -80,4 +105,3 @@ namespace SupplyChain.Controllers
         }
     }
 }
-
